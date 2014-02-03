07afdd71b8b2a0e5c32f71629b5d8d0f467fa63b
John De La Torre <jdelatorre312@johns-mac-mini.home>
2014-02-03 10:56:39 -0500

    Initial Commit

---
+++ Fraction_Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fraction_Calculator
+//
+//  Created by John De La Torre on 2/3/14.
+//  Copyright (c) 2014 John De La Torre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fraction_Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Fraction_Calculator
+//
+//  Created by John De La Torre on 2/3/14.
+//  Copyright (c) 2014 John De La Torre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
